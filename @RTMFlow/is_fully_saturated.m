9d13b21278b89ac53a1fdc0d6a6d2c513568d7a3
michaelcauson98 <michaelcauson98@googlemail.com>
2024-07-18 10:34:33 +0100

    new l-map code

--- @RTMFlow/is_fully_saturated.m
+++
@@ -1,8 +0,0 @@
-function bool = is_fully_saturated(obj)
-%% function returns a boolean to determine if the domain is fully saturated  
-
-%% check for all vent elements saturated
-vent_flag = obj.pressure_class.is_vent;
-bool = all(abs(obj.volume_fill_percentage(vent_flag)-1) < eps);
-
-end
--- @RTMFlow/update_time_level.m
+++
@@ -1,8 +0,0 @@
-function obj = update_time_level(obj)
-
-obj = obj.compute_time_increment();
-obj.time = obj.time + obj.time_step;
-obj.pressure_class.time = obj.time;
-obj.velocity_class.time = obj.time;
-
-end
